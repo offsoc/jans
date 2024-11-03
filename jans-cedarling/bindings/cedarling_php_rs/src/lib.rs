036a99a0ce536c715eba79ab4db3113caf015dfa
Olevacho <oleg@gluu.org>
2024-11-03 14:36:04 +0100

    feat(jans-cedarling): add passing policy store and app name as paramater from php side
    Signed-off-by: Olevacho <oleg@gluu.org>

--- jans-cedarling/bindings/cedarling_php_rs/src/lib.rs
+++ jans-cedarling/bindings/cedarling_php_rs/src/lib.rs
@@ -7,8 +7,6 @@ use cedarling::{
 };
 use std::collections::HashMap;
 
-static POLICY_STORE_RAW: &str = include_str!("policy-store_ok.json");
-
 #[php_class]
 pub struct Cedarling {
     cedarling: RustCedarling, // Wrap the Rust Cedarling instance
@@ -16,17 +14,17 @@ pub struct Cedarling {
 
 #[php_impl]
 impl Cedarling {
-    // Define the __construct method that PHP can use to instantiate the object
+    // Define the __construct method that PHP can use to instantiate the object with dynamic parameters
     #[php_method]
-    pub fn __construct() -> PhpResult<Self> {
-        // Initialize the Cedarling instance with the BootstrapConfig
+    pub fn __construct(policy_store_raw: &str, application_name: &str) -> PhpResult<Self> {
+        // Initialize the Cedarling instance with the BootstrapConfig using the provided parameters
         let cedarling = RustCedarling::new(BootstrapConfig {
-            application_name: "test_app".to_string(),
+            application_name: application_name.to_string(),
             log_config: LogConfig {
                 log_type: LogTypeConfig::StdOut,
             },
             policy_store_config: PolicyStoreConfig {
-                source: PolicyStoreSource::Json(POLICY_STORE_RAW.to_string()),
+                source: PolicyStoreSource::Json(policy_store_raw.to_string()),
             },
             jwt_config: JwtConfig::Disabled,
         }).map_err(|e| format!("Failed to initialize Cedarling: {:?}", e))?;
@@ -42,16 +40,14 @@ impl Cedarling {
         org_id: &str,
         userinfo_token: &str,
     ) -> PhpResult<String> {
-   /* let userinfo_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3VudHJ5IjoiVVMiLCJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJ1c2VybmFtZSI6IlVzZXJOYW1lRXhhbXBsZSIsInN1YiI6ImJvRzhkZmM1TUtUbjM3bzdnc2RDZXlxTDhMcFdRdGdvTzQxbTFLWndkcTAiLCJpc3MiOiJodHRwczovL2FkbWluLXVpLXRlc3QuZ2x1dS5vcmciLCJnaXZlbl9uYW1lIjoiQWRtaW4iLCJtaWRkbGVfbmFtZSI6IkFkbWluIiwiaW51bSI6IjhkMWNkZTZhLTE0NDctNDc2Ni1iM2M4LTE2NjYzZTEzYjQ1OCIsImNsaWVudF9pZCI6IjViNDQ4N2M0LThkYjEtNDA5ZC1hNjUzLWY5MDdiODA5NDAzOSIsImF1ZCI6IjViNDQ4N2M0LThkYjEtNDA5ZC1hNjUzLWY5MDdiODA5NDAzOSIsInVwZGF0ZWRfYXQiOjE3MjQ3Nzg1OTEsIm5hbWUiOiJEZWZhdWx0IEFkbWluIFVzZXIiLCJuaWNrbmFtZSI6IkFkbWluIiwiZmFtaWx5X25hbWUiOiJVc2VyIiwianRpIjoiZmFpWXZhWUlUMGNEQVQ3Rm93MHBRdyIsImphbnNBZG1pblVJUm9sZSI6WyJhcGktYWRtaW4iXSwiZXhwIjoxNzI0OTQ1OTc4fQ.3LTc8YLvEeb7ONZp_FKA7yPP7S6e_VTzwhvAWUJrL4M".to_string();
-    */
-    // usually ResourceData will be parse from json
-    let resource_json = serde_json::json!({
+        // Example of resource data
+        let resource_json = serde_json::json!({
             "id": "random_id",
             "type": "Jans::Issue",
-            "org_id": "some_long_id",
+            "org_id": org_id,
             "country": "US",
-    });
-    
+        });
+
         // Perform the authorization logic
         let result = self.cedarling.authorize(Request {
             access_token: access_token.to_string(),
@@ -63,7 +59,7 @@ impl Cedarling {
                 id: "random_id".to_string(),
                 resource_type: "Jans::Issue".to_string(),
                 payload: serde_json::from_value(resource_json)
-                .map_err(|err| format!("could not parse ResourceData:{err}"))?,
+                    .map_err(|err| format!("could not parse ResourceData: {err}"))?,
             },
         });
 
--- jans-cedarling/bindings/cedarling_php_rs/test.php
+++ jans-cedarling/bindings/cedarling_php_rs/test.php
@@ -1,12 +1,11 @@
 <?php
 
-$token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib0c4ZGZjNU1LVG4zN283Z3NkQ2V5cUw4THBXUXRnb080MW0xS1p3ZHEwIiwiY29kZSI6ImJmMTkzNGY2LTM5MDUtNDIwYS04Mjk5LTZiMmUzZmZkZGQ2ZSIsImlzcyI6Imh0dHBzOi8vYWRtaW4tdWktdGVzdC5nbHV1Lm9yZyIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJjbGllbnRfaWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhdWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhY3IiOiJiYXNpYyIsIng1dCNTMjU2IjoiIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSJdLCJvcmdfaWQiOiJzb21lX2xvbmdfaWQiLCJhdXRoX3RpbWUiOjE3MjQ4MzA3NDYsImV4cCI6MTcyNDk0NTk3OCwiaWF0IjoxNzI0ODMyMjU5LCJqdGkiOiJseFRtQ1ZSRlR4T2pKZ3ZFRXBvek1RIiwibmFtZSI6IkRlZmF1bHQgQWRtaW4gVXNlciIsInN0YXR1cyI6eyJzdGF0dXNfbGlzdCI6eyJpZHgiOjIwMSwidXJpIjoiaHR0cHM6Ly9hZG1pbi11aS10ZXN0LmdsdXUub3JnL2phbnMtYXV0aC9yZXN0djEvc3RhdHVzX2xpc3QifX19._eQT-DsfE_kgdhA0YOyFxxPEMNw44iwoelWa5iU1n9s";
-
 $payload_str = "some_long_id";
 
-//var_dump(cedarling_authorize_test($token,$payload_str));//we pass ID of Organization into $payload_str parameter. 
+$policy_store_raw = file_get_contents('policy-store_ok.json');
+$application_name = 'my_app_name';
 
-$cedarling = new Cedarling();
+$cedarling = new Cedarling($policy_store_raw, $application_name);
 $access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib0c4ZGZjNU1LVG4zN283Z3NkQ2V5cUw4THBXUXRnb080MW0xS1p3ZHEwIiwiY29kZSI6ImJmMTkzNGY2LTM5MDUtNDIwYS04Mjk5LTZiMmUzZmZkZGQ2ZSIsImlzcyI6Imh0dHBzOi8vYWRtaW4tdWktdGVzdC5nbHV1Lm9yZyIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJjbGllbnRfaWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhdWQiOiI1YjQ0ODdjNC04ZGIxLTQwOWQtYTY1My1mOTA3YjgwOTQwMzkiLCJhY3IiOiJiYXNpYyIsIng1dCNTMjU2IjoiIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSJdLCJvcmdfaWQiOiJzb21lX2xvbmdfaWQiLCJhdXRoX3RpbWUiOjE3MjQ4MzA3NDYsImV4cCI6MTcyNDk0NTk3OCwiaWF0IjoxNzI0ODMyMjU5LCJqdGkiOiJseFRtQ1ZSRlR4T2pKZ3ZFRXBvek1RIiwibmFtZSI6IkRlZmF1bHQgQWRtaW4gVXNlciIsInN0YXR1cyI6eyJzdGF0dXNfbGlzdCI6eyJpZHgiOjIwMSwidXJpIjoiaHR0cHM6Ly9hZG1pbi11aS10ZXN0LmdsdXUub3JnL2phbnMtYXV0aC9yZXN0djEvc3RhdHVzX2xpc3QifX19._eQT-DsfE_kgdhA0YOyFxxPEMNw44iwoelWa5iU1n9s";
 $id_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY3IiOiJiYXNpYyIsImFtciI6IjEwIiwiYXVkIjoiNWI0NDg3YzQtOGRiMS00MDlkLWE2NTMtZjkwN2I4MDk0MDM5IiwiZXhwIjoxNzI0ODM1ODU5LCJpYXQiOjE3MjQ4MzIyNTksInN1YiI6ImJvRzhkZmM1TUtUbjM3bzdnc2RDZXlxTDhMcFdRdGdvTzQxbTFLWndkcTAiLCJpc3MiOiJodHRwczovL2FkbWluLXVpLXRlc3QuZ2x1dS5vcmciLCJqdGkiOiJzazNUNDBOWVNZdWs1c2FIWk5wa1p3Iiwibm9uY2UiOiJjMzg3MmFmOS1hMGY1LTRjM2YtYTFhZi1mOWQwZTg4NDZlODEiLCJzaWQiOiI2YTdmZTUwYS1kODEwLTQ1NGQtYmU1ZC01NDlkMjk1OTVhMDkiLCJqYW5zT3BlbklEQ29ubmVjdFZlcnNpb24iOiJvcGVuaWRjb25uZWN0LTEuMCIsImNfaGFzaCI6InBHb0s2WV9SS2NXSGtVZWNNOXV3NlEiLCJhdXRoX3RpbWUiOjE3MjQ4MzA3NDYsImdyYW50IjoiYXV0aG9yaXphdGlvbl9jb2RlIiwic3RhdHVzIjp7InN0YXR1c19saXN0Ijp7ImlkeCI6MjAyLCJ1cmkiOiJodHRwczovL2FkbWluLXVpLXRlc3QuZ2x1dS5vcmcvamFucy1hdXRoL3Jlc3R2MS9zdGF0dXNfbGlzdCJ9fX0.8BwLLGkFpWGx8wGpvVmNk_Ao8nZrP_WT-zoo-MY4zqY";
 $org_id = "some_long_id";
@@ -16,28 +15,6 @@ $result = $cedarling->authz($access_token, $id_token, $org_id, $userinfo_token);
 var_dump($result);
 
 
-/*
-Later, within rust code we check : principal.org_id == resource.org_id  from cedar policy:
-
-permit(
-    principal is Jans::Workload,
-    action in [Jans::Action::"Update"],
-    resource is Jans::Issue
-)when{
-    principal.org_id == resource.org_id
-};
-
-Value ,"org_id":"some_long_id" is passwed in access token which is base64 encoded
-
-Decoded value of $token:
-
-
-decoded access_token = 
-{"alg":"HS256","typ":"JWT"}{"sub":"boG8dfc5MKTn37o7gsdCeyqL8LpWQtgoO41m1KZwdq0","code":"bf1934f6-3905-420a-8299-6b2e3ffddd6e","iss":"https://admin-ui-test.gluu.org","token_type":"Bearer","client_id":"5b4487c4-8db1-409d-a653-f907b8094039","aud":"5b4487c4-8db1-409d-a653-f907b8094039","acr":"basic","x5t#S256":"","scope":["openid","profile"],"org_id":"some_long_id","auth_time":1724830746,"exp":1724945978,"iat":1724832259,"jti":"lxTmCVRFTxOjJgvEEpozMQ","name":"Default Admin User","status":{"status_list":{"idx":201,"uri":"https://admin-ui-test.gluu.org/jans-auth/restv1/status_list"}}}
-
-
-
-*/
 
 
 
